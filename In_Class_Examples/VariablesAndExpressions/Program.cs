4939f7ee9bb01b6df529088f714a275233dc99fa
Acke9387 <adam@ou.edu>
2021-09-02 10:48:49 -0500

    Initial creation of solution for in class examples

---
+++ In_Class_Examples/VariablesAndExpressions/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace VariablesAndExpressions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstName = "Instructor";
+            string lastName = "Ackerman";
+            Console.WriteLine("Boomer Sooner!");
+
+            Console.WriteLine("What is your first name? >>");
+            firstName = Console.ReadLine();
+
+            Console.WriteLine("What is your last name? >>");
+            lastName = Console.ReadLine();
+
+            int luckyNumber = 3;
+
+            System.Console.WriteLine("What is your favorite number? >>");
+            string answer = Console.ReadLine();
+            luckyNumber = Convert.ToInt32(answer);
+
+            double evenLuckierNumber = 7 * luckyNumber;
+
+            Console.WriteLine(firstName + " " + lastName + " " + evenLuckierNumber);
+        }
+    }
+}
