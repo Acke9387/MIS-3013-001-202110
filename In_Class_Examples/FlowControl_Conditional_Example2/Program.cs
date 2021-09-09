262943c70ad4eb119e5b4449c3a86b2df9799d9a
Acke9387 <adam@ou.edu>
2021-09-09 10:57:44 -0500

    did an in class example over fizz buzz and random

---
+++ In_Class_Examples/FlowControl_Conditional_Example2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace FlowControl_Conditional_Example2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rand = new Random();
+            int randValue = rand.Next(1, 101);
+
+            if (randValue % 3 == 0 && randValue % 5 == 0)
+            {
+                Console.WriteLine("Fizzbuzz");
+            }
+            else if (randValue % 5 == 0)
+            {
+                Console.WriteLine("Buzz");
+            }
+            else if (randValue % 3 == 0)
+            {
+                Console.WriteLine("Fizz");
+            }
+            else
+            {
+                Console.WriteLine(randValue);
+            }
+
+
+        }
+    }
+}
