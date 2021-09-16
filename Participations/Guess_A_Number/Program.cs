7bfa1d289865246f590676eed47fce32ac20d466
Acke9387 <adam@ou.edu>
2021-09-16 10:42:34 -0500

    started guess a number with going over tryparse

---
+++ Participations/Guess_A_Number/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Guess_A_Number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int number;
+
+            string usersAnswer = "5";
+
+            bool isAValidNumber = int.TryParse(usersAnswer, out number);
+
+            if (isAValidNumber == false)
+            {
+                //The number is invalid
+                Environment.Exit(-10);
+            }
+            else
+            {
+                //The number is valid
+            }
+
+        }
+    }
+}
