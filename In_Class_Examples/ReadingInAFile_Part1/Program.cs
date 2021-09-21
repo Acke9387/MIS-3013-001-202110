87d70e19971b6fd19447022f6e3c9a611f6df3c7
Acke9387 <adam@ou.edu>
2021-09-21 11:38:37 -0500

    did an in class example over string manipulation and reading in a file

---
+++ In_Class_Examples/ReadingInAFile_Part1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.IO;
+
+namespace ReadingInAFile_Part1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string file = @"The Proof of The Pudding.txt";
+
+            string book = File.ReadAllText(file);
+
+            Console.WriteLine(book);
+        }
+    }
+}
---
+++ In_Class_Examples/StringManipulation/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace StringManipulation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstName = "hENRY";
+
+            Console.WriteLine(firstName);
+
+            firstName = firstName.ToLower();
+
+            Console.WriteLine(firstName);
+
+            firstName = "" + firstName.ToUpper()[0] + firstName.Substring(1);
+            Console.WriteLine(firstName);
+
+            string name = "henry james";
+            string fname = "" + name.ToUpper()[0] + name.Substring(1, name.IndexOf(' '));
+            string lname = "" + name.ToUpper()[name.IndexOf(' ') + 1] + name.Substring(name.IndexOf(' ') + 2) ;
+
+            Console.WriteLine($"The space is located at position {name.IndexOf(' ')}");
+            Console.WriteLine(fname + lname);
+
+        }
+    }
+}
