bbdb79b170a0338cdbf5cf5e1dada2f17f70ad67
Acke9387 <adam@ou.edu>
2021-10-14 11:03:06 -0500

    finished the min/max/avg participagtion and started the processing a csv file

--- Participations/Collections_Min_Max_Avg/Program.cs
+++ Participations/Collections_Min_Max_Avg/Program.cs
@@ -8,29 +8,73 @@ namespace Collections_Min_Max_Avg
         static void Main(string[] args)
         {
             List<double> grades = new List<double>();
-
+            Dictionary<double, int> gradeCount = new Dictionary<double, int>();
             do
             {
                 Console.WriteLine("Please enter a grade >>");
                 double grade = Convert.ToDouble(Console.ReadLine());
                 grades.Add(grade);
 
+                if (gradeCount.ContainsKey(grade) == false)
+                {
+                    gradeCount.Add(grade, 1);
+                }
+                else
+                {
+                    gradeCount[grade] = gradeCount[grade] + 1; ;
+                }
+
                 Console.WriteLine("Do you have another grade to enter? yes or no >>");
 
             } while (Console.ReadLine().ToLower() == "yes");
 
             double min = grades[0];
+            double max = grades[0];
+            double sum = 0;
+
+            int mode = 0;
+
+            foreach (double grade in gradeCount.Keys)
+            {
+                if (mode < gradeCount[grade])
+                {
+                    mode = gradeCount[grade];
+                }
+            }
+
+            //for (int i = 0; i < grades.Count; i++)
+            //{
+            //    double grade = grades[i];
+            //    if (grade < min)
+            //    {
+            //        min = grade;
+            //    }
+
+            //    if (grade > max)
+            //    {
+            //        max = grade;
+            //    }
+
+            //    sum += grade;
+            //}
 
             foreach (double grade in grades)
             {
 
-                if (SOME_CONDITION)
+                if (grade < min)
                 {
-                    min = ?;
+                    min = grade;
                 }
 
+                if (grade > max)
+                {
+                    max = grade;
+                }
 
+                sum += grade;
             }
+
+            double average = sum / grades.Count;
 
         }
     }
---
+++ Participations/Processing_A_CSV_File/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.IO;
+
+namespace Processing_A_CSV_File
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] linesOfFile = File.ReadAllLines("sales_data_sample.csv");
+            double totalSales = 0;
+            double totalSales2003 = 0;
+
+            for (int i = 1; i < linesOfFile.Length; i++)
+            {
+                string line = linesOfFile[i];
+
+                //     0             1           2            3           4      5         6      7       8       9         10      11     12         13  
+                //ORDERNUMBER,QUANTITYORDERED,PRICEEACH,ORDERLINENUMBER,SALES,ORDERDATE,STATUS,QTR_ID,MONTH_ID,YEAR_ID,PRODUCTLINE,MSRP,PRODUCTCODE,CUSTOMERNAME,PHONE,ADDRESSLINE1,ADDRESSLINE2,CITY,STATE,POSTALCODE,COUNTRY,TERRITORY,CONTACTLASTNAME,CONTACTFIRSTNAME,DEALSIZE
+                //10107,30,95.7,2,2871,2 / 24 / 2003 0:00,Shipped,1,2,2003,Motorcycles,95,S10_1678,Land of Toys Inc.,2125557818,897 Long Airport Avenue,,NYC,NY,10022,USA,NA,Yu,Kwai,Small
+
+                string[] piecesOfLine = line.Split(',');
+                //pieces[0]  : "10107"
+                //pieces[1]  : "30"
+                //pieces[2]  : "95.7"
+                //pieces[3]  : "2"
+                //pieces[4]  : "2871"
+                //pieces[5]  : "2"
+
+                string status = piecesOfLine[6];
+                double sales = Convert.ToDouble(piecesOfLine[4]);
+                int year = Convert.ToInt32(piecesOfLine[9]);
+
+                if (status.ToLower() == "shipped")
+                {
+                    if (year == 2003)
+                    {
+                        totalSales2003 += sales;
+                    }
+
+                    totalSales += sales;
+                }
+
+            }
+
+            Console.WriteLine($"The total sales for all orders shipped is {totalSales.ToString("C")}");
+            Console.WriteLine($"The total sales for all orders shipped in 2003 is {totalSales2003.ToString("C")}");
+        }
+    }
+}
