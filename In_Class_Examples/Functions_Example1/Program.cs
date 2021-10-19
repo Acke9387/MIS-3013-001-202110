fe12145f3a2c534c9f7c94ede6a91a02aca9752c
Acke9387 <adam@ou.edu>
2021-10-19 11:34:19 -0500

    did an in class example over functions

---
+++ In_Class_Examples/Functions_Example1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Functions_Example1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double gpa = AskQuestionAndValidateAnswerIsADouble("Please enter your GPA >>");
+            string bankMessage = "Please enter your Bank Account Balance >>";
+            
+            double bankAccountBalance = AskQuestionAndValidateAnswerIsADouble(bankMessage);
+
+
+            Console.WriteLine($"Your gpa is {gpa.ToString("N")}");
+            Console.WriteLine($"Your bank account balance is {bankAccountBalance.ToString("N")}");
+
+        }
+
+        static double AskQuestionAndValidateAnswerIsADouble(string message)
+        {
+            double answer = 0;
+            string response;
+            do
+            {
+                Console.WriteLine(message);
+                response = Console.ReadLine();
+
+            } while (double.TryParse(response, out answer) == false);
+
+            return answer;
+        }
+
+    }
+}
