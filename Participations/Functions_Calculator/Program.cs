a6d672a235105fa39d471ee05e7f6afd95dde6fc
Acke9387 <adam@ou.edu>
2021-10-21 10:41:05 -0500

    Started calculator participation

---
+++ Participations/Functions_Calculator/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Functions_Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double answer = Add(5, 72.3);
+
+            Console.WriteLine(answer);
+        }
+
+        static double Add(double val1, double rightHand)
+        {
+            //double sum = 0;
+            //sum = val1 + rightHand;
+
+            //return sum;
+
+            return val1 + rightHand;
+        }
+
+    }
+}
