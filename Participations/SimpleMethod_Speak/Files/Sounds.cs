185c0d8ee8b359b26137babb63dd1afef54f9d53
Ackerman, Adam P <adamackrmn@gmail.com>
2021-11-04 10:19:42 -0500

    started method

---
+++ Participations/SimpleMethod_Speak/Files/Sounds.cs
@@ -0,0 +1,58 @@
+﻿using NAudio.Wave;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace SimpleMethod_Speak
+{
+    public static class Sounds
+    {
+        public static void Dog()
+        {
+            using (var waveOut = new WaveOutEvent())
+            using (var wavReader = new WaveFileReader(@"Files\dog.wav"))
+            {
+                waveOut.Init(wavReader);
+                waveOut.Play();
+                Console.WriteLine("Bark Bark");
+                Thread.Sleep((int)Math.Floor(wavReader.TotalTime.TotalMilliseconds) + 1);
+            }
+        }
+
+        public static void Goat()
+        {
+            using (var waveOut = new WaveOutEvent())
+            using (var wavReader = new WaveFileReader(@"Files\goat.wav"))
+            {
+                waveOut.Init(wavReader);
+                waveOut.Play();
+                Console.WriteLine("AHHHHHHHHHHH");
+                Thread.Sleep((int)Math.Floor(wavReader.TotalTime.TotalMilliseconds) + 1);
+            }
+        }
+        public static void Default()
+        {
+            using (var waveOut = new WaveOutEvent())
+            using (var wavReader = new WaveFileReader(@"Files\default.wav"))
+            {
+                waveOut.Init(wavReader);
+                waveOut.Play();
+                Console.WriteLine("Squeeky squeeky squeek");
+                Thread.Sleep((int)Math.Floor(wavReader.TotalTime.TotalMilliseconds) + 1);
+            }
+        }
+
+        public static void Monkey()
+        {
+            using (var waveOut = new WaveOutEvent())
+            using (var wavReader = new WaveFileReader(@"Files\monkey.wav"))
+            {
+                waveOut.Init(wavReader);
+                waveOut.Play();
+                Console.WriteLine("OOoo aaah aahh ahhh");
+                Thread.Sleep((int)Math.Floor(wavReader.TotalTime.TotalMilliseconds) + 1);
+            }
+        }
+    }
+}
---
+++ Participations/SimpleMethod_Speak/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace SimpleMethod_Speak
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            
+        }
+    }
+}
