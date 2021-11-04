e3e66f10234b567c2a5105feffe0d042e5398b1b
Acke9387 <adam@ou.edu>
2021-11-04 10:39:07 -0500

    Completed the simple method speak

--- Participations/SimpleMethod_Speak/Program.cs
+++ Participations/SimpleMethod_Speak/Program.cs
@@ -6,8 +6,53 @@ namespace SimpleMethod_Speak
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            do
+            {
+                Console.WriteLine("Enter an animal >>");
+                string animal = Console.ReadLine();
+
+                if (animal.ToLower() == "bye")
+                {
+                    Environment.Exit(-5);
+                }
+
+
+                string sound = Speak(animal);
+                Console.WriteLine($"{animal} makes the sound of {sound}");
+            } while (true);
             
         }
+
+        static string Speak(string input)
+        {
+            string output = "";
+
+            switch (input.ToLower())
+            {
+                case "dog":
+                    //output = "bark bark";
+                    Sounds.Dog();
+                    break;
+
+                case "monkey":
+                    //output = "ooo ahhh eee ";
+                    Sounds.Monkey();
+                    break;
+
+                case "goat":
+                    //output = "AHHHH";
+                    Sounds.Goat();
+                    break;
+
+                default:
+                    //output = "squeek squeek";
+                    Sounds.Default();
+                    break;
+            }
+
+
+            return output;
+        }
+
     }
 }
