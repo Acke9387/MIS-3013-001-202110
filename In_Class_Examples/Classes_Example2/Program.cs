857fe118e0aad2fe35d50f4f32efeed5847ffb00
Acke9387 <adam@ou.edu>
2021-11-09 11:38:59 -0600

    Did an in class example over classes using an UML

---
+++ In_Class_Examples/Classes_Example2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Classes_Example2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Rectangle r = new Rectangle(5,5);
+            Rectangle r2 = new Rectangle();
+
+            Console.WriteLine(r.area() ); 
+        }
+    }
+}
---
+++ In_Class_Examples/Classes_Example2/Rectangle.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Classes_Example2
+{
+    public class Rectangle
+    {
+
+        private int width;
+
+        private int height;
+
+        public Rectangle()
+        {
+            width = 0;
+            height = 0;
+        }
+        
+        public Rectangle(int width, int height)
+        {
+            this.width = width;
+            this.height = height;
+        }
+
+        public double area()
+        {
+            return width * height;
+        }
+
+    }
+}
