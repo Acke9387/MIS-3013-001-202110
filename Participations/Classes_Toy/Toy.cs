ebd5a7d8c340be2995e72c1077c176cbcafc70e9
Ackerman, Adam P <adamackrmn@gmail.com>
2021-11-16 10:56:04 -0600

    finished the toy participation

---
+++ Participations/Classes_Toy/Toy.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Classes_Toy
+{
+    public class Toy
+    {
+
+        public string Manufacturer { get; set; }
+
+        public string Name { get; set; }
+
+        public double Price { get; set; }
+
+        private string Notes;
+
+        public Toy()
+        {
+            Manufacturer = string.Empty;
+            Name = string.Empty;
+            Price = 0;
+            Notes = string.Empty;
+        }
+
+        public Toy(string note)
+        {
+            Manufacturer = string.Empty;
+            Name = string.Empty;
+            Price = 0;
+            Notes = note;
+        }
+
+        public Toy(string manufacturer, string name, double price, string note)
+        {
+            Manufacturer = manufacturer;
+            Name = name;
+            Price = price;
+            Notes = note;
+        }
+
+        public void AddNote(string note)
+        {
+            if (string.IsNullOrEmpty(Notes) == true)
+            {
+                Notes = note;
+            }
+            else
+            {
+                Notes += "\n" + note;
+            }
+        }
+
+        public string ReadNotes()
+        {
+            return Notes;
+        }
+        public string GetAisle()
+        {
+            Random rand = new Random();
+
+            return Manufacturer.ToUpper()[0].ToString() + rand.Next(1, 25);
+        }
+
+        public override string ToString()
+        {
+            return $"{Name} is made by {Manufacturer} and costs {Price.ToString("C")} and can be found on aisle {GetAisle()}. {Notes}";
+        }
+
+    }
+}
