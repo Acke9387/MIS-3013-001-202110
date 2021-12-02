6c747666298a13fc3914de3b7c9d81994d1ebadb
Acke9387 <adam@ou.edu>
2021-12-02 10:41:12 -0600

    Finished participation over TextBoxes

---
+++ Participations/Classes_Textbox/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Classes_Textbox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TextBox tb = new TextBox()
+            {
+                BackColor = ConsoleColor.Green,
+                ForeColor = ConsoleColor.Cyan,
+                Padding = 5,
+                Text = "Happy Holidays!"
+            };
+
+            TextBox tb1 = new TextBox()
+            {
+                BackColor = ConsoleColor.Red,
+                ForeColor = ConsoleColor.Green,
+                Padding = 15,
+                Text = "Good luck on Finals"
+            };
+
+            tb.DisplayText();
+            tb1.DisplayText();
+        }
+    }
+}
---
+++ Participations/Classes_Textbox/TextBox.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Classes_Textbox
+{
+    public class TextBox
+    {
+        public int Padding { get; set; }
+        public ConsoleColor BackColor   {get;set;}
+        public ConsoleColor ForeColor   {get;set;}
+        public string Text        {get;set;}
+
+        public TextBox()
+        {
+            Padding = 0;
+            BackColor = ConsoleColor.Black;
+            ForeColor = ConsoleColor.White;
+            Text = string.Empty;
+        }
+
+        public void DisplayText()
+        {
+            //string pad = "";
+            //for (int i = 1  ; i <= Padding; i++)
+            //{
+            //    pad += " ";
+            //}
+            Console.ForegroundColor = ForeColor;
+            Console.BackgroundColor = BackColor;
+            //Console.WriteLine($"{pad}{Text}{pad}");
+
+            Console.WriteLine($"{Text.PadLeft(Text.Length + Padding)}{Text}{Text.PadRight(Text.Length + Padding)}");
+
+        }
+
+    }
+}
